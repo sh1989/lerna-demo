fd798aa18195ad2e67296f0dabe485545481568b
Sam Hogarth <dev@samhogy.co.uk>
2017-07-04 19:15:57 +0100

    Alters admin app text

--- packages/admin/src/index.js
+++ packages/admin/src/index.js
@@ -5,7 +5,7 @@ import { Notification } from 'common';
 const AdminApp = () => (
   <div>
     <h1>Admin App</h1>
-    <Notification msg="This is the secret app!" />
+    <Notification msg="Shh! This is the secret app!" />
   </div>
 );
 
